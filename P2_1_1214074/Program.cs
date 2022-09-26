d03997427368725a66114919a697dafb150aa272
Adorian43 <adrianbimo43@gmail.com>
2022-09-26 16:17:17 +0700

    comment

--- P2_1_1214074/Program.cs
+++ P2_1_1214074/Program.cs
@@ -21,15 +21,14 @@ namespace P2_1_1214074
             Console.WriteLine("Angka Pertama : " + angkaPertama);
             Console.WriteLine("Angka Kedua : " + angkaKedua);
 
-            Console.WriteLine("20 + 5 = " + tambah);
-            Console.WriteLine("20 - 5 = " + kurang);
-            Console.WriteLine("20 * 5 = " + kali);
-            Console.WriteLine("20 / 5 = " + bagi);
+            Console.WriteLine("20 + 5 = " + tambah); // Tambah
+            Console.WriteLine("20 - 5 = " + kurang); // Kurang
+            Console.WriteLine("20 * 5 = " + kali); // Kali
+            Console.WriteLine("20 / 5 = " + bagi); // Bagi
 
 
 
-
-
+ 
         }
     }
 }
