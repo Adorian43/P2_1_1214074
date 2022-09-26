ec78928585b45ba4d5a9a480d349c12020c9a1f3
Adorian43 <adrianbimo43@gmail.com>
2022-09-26 16:07:35 +0700

    Add project files.

---
+++ P2_1_1214074/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1214074
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int angkaPertama = int.Parse(Console.ReadLine()); ;
+            int angkaKedua = int.Parse(Console.ReadLine());
+
+            int tambah = angkaPertama + angkaKedua;
+            int kurang = angkaPertama - angkaKedua;
+            int kali = angkaPertama * angkaKedua;
+            int bagi = angkaPertama / angkaKedua;
+
+            Console.WriteLine("Angka Pertama : " + angkaPertama);
+            Console.WriteLine("Angka Kedua : " + angkaKedua);
+
+            Console.WriteLine("20 + 5 = " + tambah);
+            Console.WriteLine("20 - 5 = " + kurang);
+            Console.WriteLine("20 * 5 = " + kali);
+            Console.WriteLine("20 / 5 = " + bagi);
+
+
+
+
+
+        }
+    }
+}
